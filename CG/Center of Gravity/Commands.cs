1d8ab82df64a7ea3fe658626a880636ab6e6ee2d
unknown <dave.wolfe@DAVEWOLFE03.simpsonville.ecadinc.com>
2012-01-31 21:39:56 -0500

    Initial 2013 build test

---
+++ CG/Center of Gravity/Commands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+namespace Center_of_Gravity
+{
+    
+    public class Commands
+    {
+
+        [CommandMethod("CG")]
+        public static void RunCG()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("\nCG Here");
+        }
+        
+    }
+}
---
+++ CG/Center of Gravity/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Center_of_Gravity.ViewModel;
+
+namespace Center_of_Gravity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
